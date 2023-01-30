b5fd8e0bd36bc88ff960318c4f7c269b185df6ae
woong-garden <dndwn7007@gmail.com>
2023-01-30 16:33:16 +0900

    주석 추가

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -2,22 +2,23 @@ import React from 'react';
 
 function Todo({ item, isActive, setTodos  }) {
 
-    //prev : 이전에 값
-const handleDeleteButtonClick = () => {
-    //t.id와 같지 않은 애들만 리턴
-    setTodos(prev => prev.filter(t => t.id !== item.id))
-};
+    //삭제버튼 눌렸을 경우
+    const handleDeleteButtonClick = () => {
+        //t.id와 같지 않은 애들만 리턴
+        setTodos(prev => prev.filter(t => t.id !== item.id))
+    };
 
-const handleSwitchButtonClick = () => {
-    setTodos(prev => prev.map((t) => {
-        if(t.id === item.id){
-            //구조분해할당으로 isDone에 있는 바뀔값만 바꿔줌
-            return {...t, isDone: !t.isDone};
-        }else{
-            return t;
-        }
-    }))
-};
+    const handleSwitchButtonClick = () => {
+        //prev에는 현재 상태의 state가 들어있는 것
+        setTodos(prev => prev.map((t) => {
+            if(t.id === item.id){
+                //구조분해할당으로 isDone에 있는 바뀔값만 바꿔줌
+                return {...t, isDone: !t.isDone};
+            }else{
+                return t;
+            }
+        }))
+    };
     return (
         <div style={{
             border: '1px solid black',
